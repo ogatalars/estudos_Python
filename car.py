4eda1ee4e35391571c4198d49f347ac49aec2907
Felipe Ogata <ogataogar@gmail.com>
2022-03-21 15:37:21 -0300

    Estudo com orientação à objeto em Python

---
+++ car.py
@@ -0,0 +1,21 @@
+from mailbox import NotEmptyError
+
+
+class Car:
+
+    make = None
+    model = None
+    year = None
+    color = None
+
+    def __init__(self, make, model, year, color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+
+    def drive(self):
+        print("This " + self.model + " is driving")
+
+    def stop(self):
+        print("This car is stopping")
---
+++ objetcOrientedProg.py
@@ -0,0 +1,12 @@
+# OOP
+from car import Car
+
+car_1 = Car("Chevy", "Corvette", "1955", "Red")
+car_2 = Car("Ford", "Mustang", "2022", "Black")
+
+print(car_1.make)
+print(car_1.model)
+print(car_1.color),
+print(car_2.make)
+
+car_1.drive()
